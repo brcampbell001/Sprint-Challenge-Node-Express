2620a6b99abb692ba31270ff8b1344d03f0c5739
unknown <brcampbell001@outlook.com>
2018-02-16 14:30:31 -0600

    Current Price Get + Partial Answers

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+## Questions
+
+1. What is Node?
+
++ Technically, Node is not a Javascript frameworks but many frameworks such as Express.js have been written under its umbrella. Node is basically a proto-server and processes in a loop that accepts and responds to requests. These events loops can initiate other requests such as read files etc. It makes it easy to build event-driven application servers. 
+
+2. What is Express?
+3. What is Middleware?
+4. What is an endpoint?
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* Constants and required */
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
@@ -10,13 +11,33 @@ const PORT = 3030;
 
 server.use(bodyParser.json());
 
+const PREV_URL = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
+const CURRENT_URL = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
+
+
 /* Code start */
+
+server.get('/current', (req, res) => {
+    const current = req.body.current;
+    const urlCurr = CURRENT_URL;
+    console.log(CURRENT_URL)
+    fetch(urlCurr)
+        .then(current => current.json())
+        .then(current => {
+            console.log(current);
+            res.status(STATUS_OK);
+            res.json(current);
+        })
+        .catch(error => {
+            res.status(STATUS_USER_ERROR);
+            res.json({ error: error });
+        })
+});
 
 
 /*Code end */
 
-
-
+/*The Server is Listening #creepy*/
 server.listen(PORT, err => {
     if(err) {
         console.log(`There was an error starting server: ${err}`)
